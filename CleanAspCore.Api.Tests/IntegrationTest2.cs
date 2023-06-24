dc8a4ae75324c6b4e2ed5620037d27507ece4f05
Rick <rick-van-dam@outlook.com>
2023-06-24 15:31:59 +0200

    simple integration test

--- CleanAspCore.Api.Tests/IntegrationTest2.cs
+++ CleanAspCore.Api.Tests/IntegrationTest2.cs
@@ -1,13 +1,14 @@
 using System.Net.Http.Json;
 using CleanAspCore.Api.Tests.Helpers;
 using CleanAspCore.Domain;
+using FluentAssertions;
 
 namespace CleanAspCore.Api.Tests;
 
 public class IntegrationTest2 : IntegrationTestBase
 {
     [Fact]
-    public async Task ExecuteCommand()
+    public async Task SearchJobs_ReturnsExpectedJobs()
     {
         //Arrange
         Context.Jobs.Add(new Job()
@@ -23,11 +24,17 @@ public class IntegrationTest2 : IntegrationTestBase
         var result = await Client.GetAsync("Job");
 
         result.EnsureSuccessStatusCode();
-        var job = result.Content.ReadFromJsonAsync<JobDto>();
+        var jobs = await result.Content.ReadFromJsonAsync<JobDto[]>();
 
         //Assert
-
-
+        jobs.Should().BeEquivalentTo(new[]
+        {
+            new JobDto()
+            {
+                Id = 0,
+                Name = "Foo",
+            }
+        });
     }
     
     public IntegrationTest2(PostgreSqlLifetime fixture) : base(fixture)
