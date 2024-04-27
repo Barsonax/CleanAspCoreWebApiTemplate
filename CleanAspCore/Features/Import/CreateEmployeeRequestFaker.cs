19930b13d88d38af8632aa154a7b2c2037d464a4
Rick van Dam <rick-van-dam@outlook.com>
2024-04-27 21:14:37 +0200

    cleanup

--- CleanAspCore/Features/Import/CreateEmployeeRequestFaker.cs
+++ CleanAspCore/Features/Import/CreateEmployeeRequestFaker.cs
@@ -10,7 +10,7 @@ public sealed class CreateEmployeeRequestFaker : Faker<CreateEmployeeRequest>
         UseSeed(3);
         RuleFor(x => x.FirstName, f => f.Name.FirstName());
         RuleFor(x => x.LastName, f => f.Name.LastName());
-        RuleFor(x => x.Email, f => new EmailAddress(f.Internet.Email()));
+        RuleFor(x => x.Email, f => f.Internet.Email());
         RuleFor(x => x.Gender, f => f.PickRandom("Male", "Female"));
         RuleFor(x => x.DepartmentId, f => f.Random.Guid());
         RuleFor(x => x.JobId, f => f.Random.Guid());
--- CleanAspCore/Features/Import/EmployeeFaker.cs
+++ CleanAspCore/Features/Import/EmployeeFaker.cs
@@ -31,7 +31,7 @@ public sealed class EmployeeDtoFaker : Faker<EmployeeDto>
         UseSeed(3);
         RuleFor(x => x.FirstName, f => f.Name.FirstName());
         RuleFor(x => x.LastName, f => f.Name.LastName());
-        RuleFor(x => x.Email, f => new EmailAddress(f.Internet.Email()));
+        RuleFor(x => x.Email, f => f.Internet.Email());
         RuleFor(x => x.Gender, f => f.PickRandom("Male", "Female"));
         RuleFor(x => x.DepartmentId, f => f.Random.Guid());
         RuleFor(x => x.JobId, f => f.Random.Guid());
