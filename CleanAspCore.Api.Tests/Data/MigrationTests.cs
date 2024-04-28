18126d76e501dd26296a5ac1191c43cef8becd1a
Rick van Dam <rick-van-dam@outlook.com>
2024-04-28 12:19:48 +0200

    add migration test

---
+++ CleanAspCore.Api.Tests/Data/MigrationTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using CleanAspCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Testcontainers.PostgreSql;
+
+namespace CleanAspCore.Api.Tests.Data;
+
+[FixtureLifeCycle(LifeCycle.SingleInstance)]
+[Parallelizable(ParallelScope.Self)]
+public class MigrationTests
+{
+#pragma warning disable NUnit1032
+    private PostgreSqlContainer _databaseContainer = null!;
+#pragma warning restore NUnit1032
+    private ILogger<MigrationTests> _logger = null!;
+    private AsyncServiceScope _scope;
+
+    [SetUp]
+    public void BeforeTestCase()
+    {
+        _scope = GlobalSetup.Provider.CreateAsyncScope();
+        _databaseContainer = _scope.ServiceProvider.GetRequiredService<PostgreSqlContainer>();
+        _logger = _scope.ServiceProvider.GetRequiredService<ILogger<MigrationTests>>();
+    }
+
+    [TearDown]
+    public async Task AfterTestCase()
+    {
+        await _scope.DisposeAsync();
+    }
+
+    [TestCaseSource(typeof(MigrationTestCases))]
+    public async Task MigrationsUpAndDown_NoErrors2(MigrationScript migration)
+    {
+        var databaseName = "migrationstest";
+        var createDatabaseResult = await _databaseContainer.CreateDatabase(databaseName);
+        createDatabaseResult.ExitCode.Should().Be(0, $"Error while creating database for migrations: {createDatabaseResult.Stderr}");
+
+        var migrator = new PostgreSqlMigrator(_databaseContainer, _logger, databaseName);
+        var upResult = await migrator.Up(migration);
+        upResult.ExitCode.Should().Be(0, $"Error during migration up: {upResult.Stderr}");
+        var downResult = await migrator.Down(migration);
+        downResult.ExitCode.Should().Be(0, $"Error during migration down: {downResult.Stderr}");
+        var upResult2 = await migrator.Up(migration);
+        upResult.ExitCode.Should().Be(0, $"Error during migration up2: {upResult2.Stderr}");
+    }
+
+    private sealed class MigrationTestCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            using DbContext context = new HrContext();
+            var migrations = context.GenerateMigrationScripts();
+
+            foreach (var migration in migrations)
+            {
+                yield return new TestCaseData(migration);
+            }
+        }
+    }
+}
---
+++ CleanAspCore.Api.Tests/TestSetup/PostgreSqlDatabaseExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Testcontainers.PostgreSql;
+
+namespace CleanAspCore.Api.Tests.TestSetup;
+
+public static class PostgreSqlDatabaseExtensions
+{
+    public static async Task<ExecResult> ExecScriptAsync(this PostgreSqlContainer container, string scriptContent, string database, CancellationToken ct = default)
+    {
+        var scriptFilePath = string.Join("/", string.Empty, "tmp", Guid.NewGuid().ToString("D"), Path.GetRandomFileName());
+
+        await container.CopyAsync(Encoding.Default.GetBytes(scriptContent), scriptFilePath, Unix.FileMode644, ct)
+            .ConfigureAwait(false);
+
+        var connectionString = ParseConnectionString(container.GetConnectionString());
+
+        return await container.ExecAsync(new[] { "psql", "--username", connectionString.UserId, "--dbname", database, "--file", scriptFilePath }, ct)
+            .ConfigureAwait(false);
+    }
+
+    private sealed record SqlContainerConnectionString(string UserId, string Password);
+    private static SqlContainerConnectionString ParseConnectionString(string connectionString)
+    {
+        var dic = connectionString
+            .Split(';')
+            .Select(x => x.Split('='))
+            .ToDictionary(x => x[0], x => x[1]);
+
+        return new SqlContainerConnectionString(dic["Username"], dic["Password"]);
+    }
+
+    public static async Task<ExecResult> CreateDatabase(this PostgreSqlContainer container, string name)
+    {
+        return await container.ExecScriptAsync($"CREATE DATABASE {name};");
+    }
+}
