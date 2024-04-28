50d31e0fa8746c03b0f27c90226f5e1ec5d6ad61
Rick van Dam <rick-van-dam@outlook.com>
2024-04-28 12:29:57 +0200

    cleanup

--- CleanAspCore/Program.cs
+++ CleanAspCore/Program.cs
@@ -26,11 +26,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.EnsureHrContextDatabaseIsCreated();
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
