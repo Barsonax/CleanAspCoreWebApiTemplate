bd188f3f39c1bd61ff65a07e09a2ed55789e4012
Rick van Dam <rick-van-dam@outlook.com>
2024-05-09 19:56:34 +0200

    add MicroElements.Swashbuckle.FluentValidation

--- CleanAspCore/Program.cs
+++ CleanAspCore/Program.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using CleanAspCore;
 using CleanAspCore.Data;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SupportNonNullableReferenceTypes();
 });
+builder.Services.AddFluentValidationRulesToSwagger();
 
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
