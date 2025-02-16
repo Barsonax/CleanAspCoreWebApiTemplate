379ab146aeaa5ce99f2bb75cc3ea38545e2ba3c9
Rick van Dam <rick-van-dam@outlook.com>
2025-02-16 13:42:16 +0100

    add failing test

--- Tests/CleanAspCore.Api.Tests/Endpoints/Departments/AddDepartmentsTests.cs
+++ Tests/CleanAspCore.Api.Tests/Endpoints/Departments/AddDepartmentsTests.cs
@@ -5,6 +5,13 @@ namespace CleanAspCore.Api.Tests.Endpoints.Departments;
 internal sealed class AddDepartmentsTests(TestWebApi sut)
 {
     [Test]
+    public Task Fail()
+    {
+        Assert.Fail("Should throw exception");
+        return Task.CompletedTask;
+    }
+
+    [Test]
     public async Task CreateDepartment_IsAdded()
     {
         //Arrange
