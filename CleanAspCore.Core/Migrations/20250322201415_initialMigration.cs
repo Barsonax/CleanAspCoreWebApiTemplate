eddadb0c5511a12e7012c3bb4b23c5dca66ebaa0
Rick van Dam <rick-van-dam@outlook.com>
2025-03-22 21:21:57 +0100

    dotnet format

--- CleanAspCore.Core/Migrations/20250322201415_initialMigration.cs
+++ CleanAspCore.Core/Migrations/20250322201415_initialMigration.cs
@@ -3,110 +3,109 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace CleanAspCore.Core.Migrations
+namespace CleanAspCore.Core.Migrations;
+
+/// <inheritdoc />
+public partial class initialMigration : Migration
 {
     /// <inheritdoc />
-    public partial class initialMigration : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Departments",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Departments", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Departments",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Departments", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Jobs",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Jobs", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Jobs",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Jobs", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Weapons",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Type = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
-                    Range = table.Column<float>(type: "real", nullable: true),
-                    Bow_Damage = table.Column<float>(type: "real", nullable: true),
-                    Bow_RateOfFire = table.Column<float>(type: "real", nullable: true),
-                    Damage = table.Column<float>(type: "real", nullable: true),
-                    RateOfFire = table.Column<float>(type: "real", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Weapons", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Weapons",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Type = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                Range = table.Column<float>(type: "real", nullable: true),
+                Bow_Damage = table.Column<float>(type: "real", nullable: true),
+                Bow_RateOfFire = table.Column<float>(type: "real", nullable: true),
+                Damage = table.Column<float>(type: "real", nullable: true),
+                RateOfFire = table.Column<float>(type: "real", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Weapons", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Employees",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Gender = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    DepartmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    JobId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Employees", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Employees_Departments_DepartmentId",
-                        column: x => x.DepartmentId,
-                        principalTable: "Departments",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Employees_Jobs_JobId",
-                        column: x => x.JobId,
-                        principalTable: "Jobs",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Employees",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Gender = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                DepartmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                JobId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Employees", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Employees_Departments_DepartmentId",
+                    column: x => x.DepartmentId,
+                    principalTable: "Departments",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Employees_Jobs_JobId",
+                    column: x => x.JobId,
+                    principalTable: "Jobs",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Employees_DepartmentId",
-                table: "Employees",
-                column: "DepartmentId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Employees_DepartmentId",
+            table: "Employees",
+            column: "DepartmentId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Employees_JobId",
-                table: "Employees",
-                column: "JobId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Employees_JobId",
+            table: "Employees",
+            column: "JobId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Employees");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Employees");
 
-            migrationBuilder.DropTable(
-                name: "Weapons");
+        migrationBuilder.DropTable(
+            name: "Weapons");
 
-            migrationBuilder.DropTable(
-                name: "Departments");
+        migrationBuilder.DropTable(
+            name: "Departments");
 
-            migrationBuilder.DropTable(
-                name: "Jobs");
-        }
+        migrationBuilder.DropTable(
+            name: "Jobs");
     }
 }
