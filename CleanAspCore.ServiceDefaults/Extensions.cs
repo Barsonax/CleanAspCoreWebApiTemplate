8f36d66ccb5c58d09989c291b12dc70ec757fe71
Rick van Dam <rick-van-dam@outlook.com>
2025-03-23 06:24:52 +0100

    fix warning

--- CleanAspCore.ServiceDefaults/Extensions.cs
+++ CleanAspCore.ServiceDefaults/Extensions.cs
@@ -18,7 +18,7 @@ namespace CleanAspCore.ServiceDefaults;
 // Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
-public static class Extensions
+public static class ServiceDefaultsExtensions
 {
     public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
